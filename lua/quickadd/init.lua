bc934bf13dc1566d69962e6a3d46580bd3eb34fd
Yannick Lehnhausen <yannick.lehnhausen@janitza.de>
2025-06-25 08:59:07 +0200

    link filename for todo

--- lua/quickadd/init.lua
+++ lua/quickadd/init.lua
@@ -144,16 +144,8 @@ function M.save_todo(content)
 	utils.ensure_dir_exists(vim.fn.fnamemodify(M.config.todo_path, ":h"))
 
 	-- Format the todo entry
-	local current_file = utils.get_filename(M.last_buffer)
-	local todo_text = ""
-
-	if string.find(current_file, "[[") then
-		todo_text =
-			string.format("- [ ] [[%s]] : %s [Created:: %s]\n", utils.get_date_link(), content, utils.get_timestamp())
-	else
-		todo_text =
-			string.format("- [ ] [[%s]] : %s [Created:: %s]\n", utils.get_date_link(), content, utils.get_timestamp())
-	end
+	local filename = utils.get_filename(M.last_buffer)
+	local todo_text = string.format("- [ ] [[%s]] : %s [Created:: %s]\n", filename, content, utils.get_timestamp())
 
 	local file = io.open(M.config.todo_path, "a")
 	if file then
